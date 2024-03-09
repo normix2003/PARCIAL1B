2b6f683fa4656d4b61f5f749fbfa7eb2fe3f67cd
normix2003 <arrivillagariveran@gmail.com>
2024-03-09 10:46:53 -0600

    Agregar archivos de proyecto.

---
+++ PARCIAL1B/models/ElementosContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PARCIAL1B.models
+{
+    public class ElementosContext: DbContext
+    {
+
+        public ElementosContext(DbContextOptions<ElementosContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Equipos> Equipos { get; set; }
+
+
+        
+    }
+}
