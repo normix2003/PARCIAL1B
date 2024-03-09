5e578f3092c1643da7fad874f1715680d5ac56ed
Carlos Alejandro Murga <murgamartinezc@gmail.com>
2024-03-09 11:26:52 -0600

    Controlador para platos

---
+++ PARCIAL1B/Controllers/PlatosController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PARCIAL1B.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PARCIAL1B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatosController : ControllerBase
+    {
+        private readonly ElementosContext _elementosContext;
+
+        public PlatosController(ElementosContext elementosContext)
+        {
+            _elementosContext = elementosContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get() 
+        { 
+            List<Platos> listadoPlatos = (from e in _elementosContext.Platos
+                                          select e).ToList();
+            
+            if (listadoPlatos.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoPlatos);
+        }
+    }
+}
