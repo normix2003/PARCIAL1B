10114b56d15812b5693978b05d9a5c132d23ef2a
normix2003 <arrivillagariveran@gmail.com>
2024-03-09 11:23:37 -0600

    modeslos con datos

--- PARCIAL1B/models/Platos.cs
+++ PARCIAL1B/models/Platos.cs
@@ -1,6 +1,22 @@
-﻿namespace PARCIAL1B.models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PARCIAL1B.models
 {
     public class Platos
     {
+        public class Elementos
+        {
+            [Key]
+            public int PlatoID { get; set; }
+
+            public int? EmpresaID { get; set; }
+            public int? GrupoID { get; set; }
+
+            public string NombrePlato { get; set; }
+            public string DescripcionPlato { get; set; }
+            public float? Precio { get; set; }
+          
+
+        }
     }
 }
