d3af96327f1b8ac8cd636eae0352f86190708ffd
Carlos Alejandro Murga <murgamartinezc@gmail.com>
2024-03-09 11:43:43 -0600

    Cambios en Program

--- PARCIAL1B/Program.cs
+++ PARCIAL1B/Program.cs
@@ -1,3 +1,6 @@
+using PARCIAL1B.models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
